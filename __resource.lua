9d64c9692a9fcf025c2f2abaae6ab8782d72b6cc
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2020-07-25 18:39:06 -0600

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,29 @@
+
+--[[
+	Bundled from:
+		HG-Anticheat: https://github.com/HackerGeo-sp1ne/HG_AntiCheat
+		FiveM-BanSql: https://github.com/RedAlex/FiveM-BanSql
+]]
+
+description 'ToxicAntiCheat'
+
+client_scripts {
+    '@es_extended/locale.lua',
+    'locales/en.lua',
+    'config.lua',
+    'toxic-anticheat-cl.lua'
+}
+
+server_scripts {
+    '@es_extended/locale.lua',
+	'@async/async.lua',
+	'@mysql-async/lib/MySQL.lua',
+	'locales/en.lua',
+	'config.lua',
+    'toxic-anticheat-sv.lua'
+}
+
+dependencies {
+    'essentialmode',
+    'async'
+}
